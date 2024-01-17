af0b715cc67a57c138ef97a76474da5e079aba58
pkm0707 <mpraveenkumar@student.tce.edu>
2024-01-17 17:04:29 +0530

    final commit

--- src/page/Contact.js
+++ src/page/Contact.js
@@ -1,7 +1,21 @@
 import React from "react";
-
+import "./Contact.css"
 const Contact = () => {
-  return <div>Contact</div>;
+  return (
+    <div class="container">
+      <div class="contact-box">
+        <div class="left"></div>
+        <div class="right">
+          <h2>Contact Us</h2>
+          <input type="text" class="field" placeholder="Your Name"/>
+          <input type="text" class="field" placeholder="Your Email"/>
+          <input type="text" class="field" placeholder="Phone"/>
+          <textarea placeholder="Message" class="field"></textarea>
+          <button class="btn">Send</button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Contact;
--- src/page/Home.js
+++ src/page/Home.js
@@ -42,7 +42,7 @@ const Home = () => {
             />
           </div>
           <h2 className="text-4xl md:text-7xl font-bold py-3">
-            The Fastest Delevery in{" "}
+            The Fastest Delivery in{" "}
             <span className="text-red-600 "> Your Home</span>
           </h2>
           <p className="py-3 text-base ">
